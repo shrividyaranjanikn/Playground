3280ec8d15c10bef5b2899ee2b6276df1d006b70
shrividyaranjanikn <shrividyaranjanikn@gmail.com>
2020-03-15 17:24:22 -0500

    Add app and variables files

---
+++ Bob_Tabor_Javascript/variables.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 11;
+let c = a+b;
+console.log('The value of c is:' + c);
+// OR
+var d = 12;
+var e = 2;
+var sum = d+e;
+console.log('Sum = '+sum);
+let firstName = 'Shrividya';
+let lastName = 'Ranjani';
+console.log(firstName + lastName);
