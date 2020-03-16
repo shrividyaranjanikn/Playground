691f664d1cfba51ab79391b89dd8769cd6e1b0fb
shrividyaranjanikn <shrividyaranjanikn@gmail.com>
2020-03-16 14:14:09 -0500

    Add operators

---
+++ Bob_Tabor_Javascript/operators.js
@@ -0,0 +1,45 @@
+// Types of operators
+
+// Assignment operator
+let a = 4;
+// Arithmetic operator
+let b = 5;
+let c = a+b;
+var m = 10 % 3;
+console.log('The value of a-b is: '+(a-b));
+console.log('The value of a*b is: '+(a*b));
+console.log('The value of a/b is: '+(a/b));
+console.log('The value of c is: '+c);
+console.log('The value of m is: '+m);
+// Increment/Decrement
+
+//Increment
+var d = 1;
+d++;
+console.log(++d);
+console.log(d);
+// Decrement
+var e = 2;
+--e;
+console.log(e--);
+console.log(e);
+
+// String operators + used for concatinating two strings, '' to enclose the string
+let q = 'Hello!';
+let w = ' How are you?';
+console.log(q+w);
+
+//Precedence
+var u = (1+2)*3;
+console.log(u);
+
+// Funtion invocation operators ()
+//console.log('');
+
+// Logical operator and : && or : ||
+
+// Member accessor operator . ; console.log()
+
+// Code block operators {}
+
+// Array element access operators []
