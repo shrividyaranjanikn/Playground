e8b8fadc6412b97ebc2db25a45a131e603cf0849
shrividyaranjanikn <shrividyaranjanikn@gmail.com>
2020-03-16 13:56:33 -0500

    Add expressions file

---
+++ Bob_Tabor_Javascript/expressions.js
@@ -0,0 +1,14 @@
+// Types of expression
+
+// Variable declaration
+let a;
+
+// Assigning a value
+a = 4;
+
+// Perform an evaluation that return a single value
+
+let b = 1;
+let c = 2;
+let d = b+c;
+console.log(d);
