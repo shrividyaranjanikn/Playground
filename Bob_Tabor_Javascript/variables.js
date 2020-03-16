154352f5095079747b017b15f8b61bd4fa424343
shrividyaranjanikn <shrividyaranjanikn@gmail.com>
2020-03-16 13:53:37 -0500

    Add comments

--- Bob_Tabor_Javascript/variables.js
+++ Bob_Tabor_Javascript/variables.js
@@ -1,12 +1,18 @@
+// Variable declaration and initialization
+
+// Using let keyword
 let a = 10;
 let b = 11;
 let c = a+b;
 console.log('The value of c is:' + c);
-// OR
+
+// Using var keyword
 var d = 12;
 var e = 2;
 var sum = d+e;
 console.log('Sum = '+sum);
+
+// Concatinating two strings
 let firstName = 'Shrividya';
 let lastName = 'Ranjani';
 console.log(firstName + lastName);
