a67fc130f69c62b61b204ae3952c9119dde5f459
shrividyaranjanikn <shrividyaranjanikn@gmail.com>
2020-03-17 17:58:09 -0500

    Add functions

---
+++ Bob_Tabor_Javascript/function.js
@@ -0,0 +1,26 @@
+// Functions returning a function
+
+// function returing a value
+
+/*function one(){
+    return 'one';
+}
+
+let result = one();
+console.log(result);
+
+let r = one;
+console.log(typeof r);
+
+let value = one;
+console.log(value());*/
+
+// Function returing a function
+function two(){
+    return function(){
+        console.log('two');
+    }
+}
+
+let myFunction = two();
+myFunction();
