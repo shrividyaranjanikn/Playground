ca4667eaca305ee2ea89846f79c1956c0457d136
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-25 21:31:52 -0500

    add stack program

---
+++ JavaScript/Stack.js
@@ -0,0 +1,35 @@
+class Stack{
+
+    constructor(){
+        this.MAX_LEN = 30;
+        this.topIndex = -1;
+        this.container = [];
+    }
+
+    push(element){
+        if(topIndex == this.MAX_LEN-1){
+            throw new Error("Stack full");
+        }
+
+        this.topIndex += 1;
+        this.container[topIndex] = element;
+    }
+
+    pop(){
+        if(topIndex == -1){
+            throw new Error("Stack Empty");
+        }
+
+        poppedElement = this.container[topIndex];
+        this.topIndex -= 1;
+        return poppedElement;
+    }
+
+    getTopElement(){
+        if(topIndex == -1){
+            throw new Error("Stack Empty");
+        }
+
+        return this.container[topIndex];
+    }
+}
