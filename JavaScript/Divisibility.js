20ce31d317e771846c9d081cbbafa3a3e1efc913
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-27 23:02:16 -0500

    modify formatting

--- JavaScript/Divisibility.js
+++ JavaScript/Divisibility.js
@@ -1,6 +1,6 @@
 // Write a program to check whether a number is divisible by 5 and 11 or not.
 
-function divisibility(x){
+function divisibility(x) {
     if(x % 5 == 0 && x % 11 == 0){
         console.log(`${x} is divisible by 5 and 11`);
     }
