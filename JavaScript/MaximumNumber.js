fd1a9d40ea83fe43a117e80bb7fe37f0fcfe58e8
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-27 22:48:11 -0500

    modify console.log contents

--- JavaScript/MaximumNumber.js
+++ JavaScript/MaximumNumber.js
@@ -2,10 +2,10 @@
 
 function maximum(x,y){
     if(x > y){
-        console.log("x is greater than y");
+        console.log(`${x} is greater than ${y}`);
     }
     else{
-        console.log("y is greater than x");
+        console.log(`${y} is greater than ${x}`);
     }
 }
 
--- JavaScript/Maximum_of_three.js
+++ JavaScript/Maximum_of_three.js
@@ -2,15 +2,15 @@
 
 function maxOfThree (x,y,z){
     if(x > y && x > z){
-        console.log("x is greater than y and z");
+        console.log(`${x} is greater than ${y} and ${z}`);
     }
 
     else if(y > x && y > z){
-        console.log("y is greater than x and z");
+        console.log(`${y} is greater than ${x} and ${z}`);
     }
 
     else{
-        console.log("z is greater than x and y");
+        console.log(`${z} is greater than ${x} and ${y}`);
     }
 } 
 
