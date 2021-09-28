2633a97ff38504d08bbef1b4cbd90539e754a871
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-27 23:38:48 -0500

    add maximum number using conditional operator program

---
+++ JavaScript/max.js
@@ -0,0 +1,8 @@
+// Write a program to find maximum between two numbers using conditional operator.
+
+function max(x,y){
+    (x > y) ? console.log(`${x} is greater than ${y}`) : console.log(`${y} is greater than ${x}`);
+}
+
+max(23,54);
+max(45,24);
