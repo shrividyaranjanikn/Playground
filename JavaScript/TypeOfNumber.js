a9581f5f54da1ee1fde63b55d53b9a0d787d2b35
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-27 22:30:48 -0500

    add type of number program

---
+++ JavaScript/TypeOfNumber.js
@@ -0,0 +1,17 @@
+// Write a program to check whether a number is negative, positive or zero.
+
+function typeOfNumber(x){
+    if(x < 0){
+        console.log("x is negative");
+    }
+    else if(x == 0){
+        console.log("x is zero");
+    }
+    else{
+        console.log("x is positive");
+    }
+}
+
+typeOfNumber(10);
+typeOfNumber(-45);
+typeOfNumber(0);
