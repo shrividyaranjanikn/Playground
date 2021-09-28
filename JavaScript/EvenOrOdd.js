3d5a6fc45988a1675c9ce73e15c23cb10b41a262
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-27 23:02:28 -0500

    add even or odd program

---
+++ JavaScript/EvenOrOdd.js
@@ -0,0 +1,14 @@
+// Write a program to check whether a number is even or odd.
+
+function evenOrOdd(x) {
+    if(x % 2 == 0){
+        console.log(`${x} is even`);
+    }
+    else{
+        console.log(`${x} is odd`);
+    }
+}
+
+evenOrOdd(4);
+evenOrOdd(78);
+evenOrOdd(5);
