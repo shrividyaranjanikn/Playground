93bc6b9b0fc8d37443fe28ba6a474dc327d540a7
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-27 22:14:07 -0500

    add maximum of two numbers program

---
+++ JavaScript/MaximumNumber.js
@@ -0,0 +1,13 @@
+// Write a program to find maximum between two numbers.
+
+function maximum(x,y){
+    if(x > y){
+        console.log("x is greater than y");
+    }
+    else{
+        console.log("y is greater than x");
+    }
+}
+
+let x,y;
+maximum(8,5);
