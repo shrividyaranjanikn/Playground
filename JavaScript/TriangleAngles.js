81f2a41a4b834b804344468853eb6fe0ee3359c1
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-28 23:36:06 -0500

    add user input code

--- JavaScript/TriangleAngles.js
+++ JavaScript/TriangleAngles.js
@@ -1,8 +1,8 @@
 // Write a program to input angles of a triangle and check whether triangle is valid or not.
 
-function isValidTriangle(x, y, z) {
+function isValidTriangle(angle1, angle2, angle3) {
 
-    if(x + y + z == 180){
+    if(angle1 + angle2 + angle3 == 180){
         console.log("This is a valid triangle.");
     }
 
@@ -11,3 +11,24 @@ function isValidTriangle(x, y, z) {
     }
 }
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  readline.question('Enter the number of units: ', angle1 => {
+    console.log(`The first angle of a triangle is ${angle1}!`);
+    readline.close();
+  });
+
+  readline.question('Enter the number of units: ', angle2 => {
+    console.log(`The first angle of a triangle is ${angle2}!`);
+    readline.close();
+  });
+
+  readline.question('Enter the number of units: ', angle3 => {
+    console.log(`The first angle of a triangle is ${angle3}!`);
+    readline.close();
+  });
+
+  
