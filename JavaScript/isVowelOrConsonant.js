106413cfd7d838f36bfc62e57ae8a3cc4b9a46bd
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-29 00:45:58 -0500

    reduce the code content

--- JavaScript/isVowelOrConsonant.js
+++ JavaScript/isVowelOrConsonant.js
@@ -5,45 +5,18 @@ function isVowelOrConsonant(character) {
     switch(character){
         
         case 'a':
-            console.log(`${character} is a vowel!`);
-            break;
-        
         case 'A':
-            console.log(`${character} is a vowel!`);
-            break;
-
         case 'e':
-            console.log(`${character} is a vowel!`);
-            break;
-
         case 'E':
-            console.log(`${character} is a vowel!`);
-            break;
-
         case 'i':
-            console.log(`${character} is a vowel!`);
-            break;
-        
         case 'I':
-            console.log(`${character} is a vowel!`);
-            break;
-
         case 'o':
-            console.log(`${character} is a vowel!`);
-            break;
-        
         case 'O':
-            console.log(`${character} is a vowel!`);
-            break;
-        
         case 'u':
-            console.log(`${character} is a vowel!`);
-            break;
-
         case 'U':
             console.log(`${character} is a vowel!`);
             break;
-        
+    
         default:
             console.log(`${character} is a consonant!`);
     }
