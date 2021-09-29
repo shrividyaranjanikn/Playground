9cc2a02a10a84b96f05bf8ac6a419b93eda8b26e
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-28 23:30:31 -0500

    add user input code

--- JavaScript/ElectricityBill.js
+++ JavaScript/ElectricityBill.js
@@ -4,3 +4,17 @@ For next 100 units Rs. 0.75/unit
 For next 100 units Rs. 1.20/unit
 For unit above 250 Rs. 1.50/unit
 An additional surcharge of 20% is added to the bill */
+
+function electricityBill() {
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+
+      readline.question('Enter the number of units: ', units => {
+        console.log(`The number of units is ${units}!`);
+        readline.close();
+      });
+}
+
+electricityBill();
