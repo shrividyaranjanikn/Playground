3ab8c3a38d040498056c24c17f841563d3e86cc4
Shrividya <shrividyaranjanikn@gmail.com>
2021-09-28 23:33:59 -0500

    add triangle validation code

---
+++ JavaScript/TriangleAngles.js
@@ -0,0 +1,13 @@
+// Write a program to input angles of a triangle and check whether triangle is valid or not.
+
+function isValidTriangle(x, y, z) {
+
+    if(x + y + z == 180){
+        console.log("This is a valid triangle.");
+    }
+
+    else{
+        console.log("This is not a valid triangle.");
+    }
+}
+
