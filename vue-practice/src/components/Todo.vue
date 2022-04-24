71f5c94d2a2d3787da9ca1c26daf9dd93b006bbc
Shrividya <shrividyaranjanikn@gmail.com>
2022-04-24 16:22:39 -0500

    add functionality for add button

--- vue-practice/src/components/Todo.vue
+++ vue-practice/src/components/Todo.vue
@@ -1,12 +1,39 @@
 <script>
+
+let id = 0
+
 export default {
-  d
+  data() {
+    return {
+      newTodo: '',
+      todos: [
+        {
+          id:id++,
+          text:"Learn Vue JS"
+        }
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id:id++,
+        text: this.newTodo
+      })
+      this.newTodo = ''
+    }
+  }
 }
 </script>
 
 <template>
   <div id="textbox">
-    <input type="text"/>
-    <button>Add</button>
+    <form @submit.prevent="addTodo">
+      <input v-model="newTodo" type="text"/>
+      <button @click="addTodo" placeholder="Enter the task here">Add</button>
+    </form>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">{{todo.text}}</li>
+    </ul>   
   </div>
 </template>
