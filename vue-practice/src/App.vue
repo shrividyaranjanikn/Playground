4c5c785bbbcccf822fb01733a87b4f03546e5613
Shrividya <shrividyaranjanikn@gmail.com>
2022-04-24 15:08:17 -0500

    add todo application skeleton

--- vue-practice/src/App.vue
+++ vue-practice/src/App.vue
@@ -1,19 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Todo from './components/Todo.vue';
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Todo />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
+    
   </main>
 </template>
 
@@ -72,6 +71,7 @@ a,
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
+    flex-direction: column;
   }
 
   .logo {
---
+++ vue-practice/src/components/Todo.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  d
+}
+</script>
+
+<template>
+  <div id="textbox">
+    <input type="text"/>
+    <button>Add</button>
+  </div>
+</template>
